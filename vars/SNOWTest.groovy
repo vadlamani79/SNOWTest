2245ffac430d00ccc0c9d08194368ceee73e5a05
Vadlamani <847336@bah.com>
2022-11-20 21:57:29 -0500

    new commmit

---
+++ vars/SNOWTest.groovy
@@ -0,0 +1,15 @@
+def call(string testName){
+  
+if ("${testName}" == "create")
+     {
+       sh """curl --location --request POST 'https://dev104644.service-now.com/api/sn_chg_rest/change/normal' --header 'Content-Type: application/json' --header 'Authorization: Basic YWRtaW46NnpxQE93QGUwSEND' --data-raw '{"short_description": "Test Desc 1233","assignment_group": "Database"}'"""
+     }
+  else if ("${testName}" == "update")
+     {
+       sh ""
+     }
+  else if ("${testName}" == "get")
+     {
+       sh """curl --location --request GET 'https://dev104644.service-now.com/api/sn_chg_rest/change/20de36ec73d423002728660c4cf6a7d6' --header 'Authorization: Basic YWRtaW46NnpxQE93QGUwSEND'"""
+     }
+}
